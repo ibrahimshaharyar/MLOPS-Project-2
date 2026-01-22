b08a0a5b265e0f1e31ee57111525b6b54b5e3695
ibrahimshaharyar <ibrahimshaharyar41@gmail.com>
2026-01-23 02:52:53 +0500

    Fix: add load_and_prepare_image to image_ops

---
+++ src/utils/image_ops.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+
+def load_and_prepare_image(image_path: str, image_size: int) -> tf.Tensor:
+    img_bytes = tf.io.read_file(image_path)
+    img = tf.image.decode_image(img_bytes, channels=3, expand_animations=False)
+    img = tf.image.resize(img, (image_size, image_size))
+    img = tf.cast(img, tf.float32)  # keep float, model handles preprocess_input inside
+    img = tf.expand_dims(img, axis=0)  # (1, H, W, 3)
+    return img
